63ca83387eac04ca54b9e6317788db3f440d9dd9
HARSHIT SHARMA <harsh007fc@gmail.com>
2022-05-15 19:00:23 +0530

    Frontend Done

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return <footer>
+      <Container>
+          <Row>
+              <Col className='text-center py-3'>
+                  Copyright &copy; DigiStore
+              </Col>
+          </Row>
+          {/* <Row className='text-center py-3'>
+                Made with ♥
+           </Row> */}
+      </Container>
+  </footer>
+  
+}
+
+export default Footer
