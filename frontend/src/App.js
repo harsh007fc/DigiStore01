8af0f8b10fbc6be80c705aee9c48fef5050e468f
HARSHIT SHARMA <harsh007fc@gmail.com>
2022-05-21 02:19:36 +0530

    Added more code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 
 const App = () => {
@@ -14,8 +15,9 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen/>} ></Route>
-            <Route path='/product/:id' element={<ProductScreen/>} ></Route>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id' element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './bootstrap.min.css';
+import { Provider } from 'react-redux'
+import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import phone from '../public/images/phone.jpeg'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
