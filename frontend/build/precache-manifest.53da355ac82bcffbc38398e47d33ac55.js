b16d59a66ccd1e3c52c158be549449c9df595f53
HARSHIT SHARMA <harsh007fc@gmail.com>
2022-05-26 16:21:09 +0530

    Added Build folder

---
+++ frontend/build/precache-manifest.53da355ac82bcffbc38398e47d33ac55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dd1f6e2c394ed81d87ca4f40c076ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8365ebc5f28f2d26246a",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8365ebc5f28f2d26246a",
+    "url": "/static/js/main.7b03e725.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
